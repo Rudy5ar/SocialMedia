8ab45630376940b9f5b70c13a99c4686f3042d18
Petar Rudovic <petar.rudovic@itengine.rs>
2024-08-05 11:14:07 +0200

    Initial commit

---
+++ src/main/java/com/socialmedia/socialmediaclone/SocialMediaCloneApplication.java
@@ -0,0 +1,13 @@
+package com.socialmedia.socialmediaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/socialmedia/socialmediaclone/SocialMediaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmedia.socialmediaclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
