bfddcd0864672687d624f1fc3fd98117e8cd9d63
Petar Rudovic <petar.rudovic2@gmail.com>
2024-09-25 11:29:53 +0200

    Quick migration fix

--- src/main/resources/db/migration/V1_1_4__reply_sample_data.sql
+++ src/main/resources/db/migration/V1_1_4__reply_sample_data.sql
@@ -1,8 +1,8 @@
 
 INSERT INTO `CommentReply` (`idCommentReply`, `text`, `numOfLikes`, `idComment`, `idUser`)
-VALUES (1, 'This is a reply to comment 1', 10, 1, 11),
-       (2, 'Another reply to comment 1', 5, 2, 11),
-       (3, 'Reply to comment 2', 3, 3, 11);
+VALUES (1, 'This is a reply to comment 1', 10, 1, 1),
+       (2, 'Another reply to comment 1', 5, 2, 1),
+       (3, 'Reply to comment 2', 3, 3, 1);
 
 INSERT INTO `LikedReply` (`idLikedReply`, `idUser`, `idCommentReply`)
 VALUES (1, 1, 1),
